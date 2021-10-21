88b33d5965149c046d4fb468539cb109d422321d
Ivan Cvitkovic <ivanc@uw.edu>
2021-10-21 14:55:50 -0700

    Iterate over given services; cleanup

--- bin/backup-service.sh
+++ bin/backup-service.sh
@@ -8,7 +8,7 @@ repo_path="${bin_path}/.."
 usage() {
     cat << USAGE >&2
 Usage:
-    $cmdname [-h] [-b backup_location]
+    $cmdname [-h] [-b backup_location] compose_project_dir [compose_project_services...]
     -h     Show this help message
     -b     Override default backup location (/tmp)
 
@@ -37,8 +37,47 @@ shift $((OPTIND-1))
 default_backups_dir=/tmp
 BACKUPS_DIR="${backups_dir:-$default_backups_dir}"
 
+COMPOSE_PROJECT_DIR="$1"
+# remove arg from argv
+shift
+
+# remaining args are docker-compose services
+COMPOSE_PROJECT_SERVICES=$@
+
+# if none given, back up all
+if [ -z "$COMPOSE_PROJECT_SERVICES" ]; then
+    COMPOSE_PROJECT_SERVICES="keycloak fhir"
+fi
+
+
+get_compose_project() {
+    # get path to first active docker-compose project, given project name
+    local repo_path="$1"
+    local compose_project_dir_name="$2"
+    # assume project is in use based on presence of .env file
+    echo "$(dirname $(find "$repo_path" -path "*/$compose_project_dir_name/.env" -print -quit))"
+}
+
+dump_db() {
+    # get path to first active docker-compose project, given project name
+    local db_name="$1"
+    local dump_filename="$2"
+
+    echo Backing up $db_name...
+    docker-compose exec -T --user postgres db bash -c "\
+        pg_dump \
+            --dbname $db_name \
+            --no-acl \
+            --no-owner \
+            --encoding utf8 "\
+    > "${BACKUPS_DIR}/${dump_filename}-${db_name}.sql"
+    echo "Backup written to ${BACKUPS_DIR}/${dump_filename}-${db_name}.sql"
+}
+
+
+docker_compose_directory="$(get_compose_project "$repo_path" "$COMPOSE_PROJECT_DIR")"
+
 # docker-compose commands must be run in the same directory as docker-compose.yaml
-docker_compose_directory="${repo_path}/docker"
 cd "${docker_compose_directory}"
 
 if [ -z "$(docker-compose ps --quiet db)" ]; then
@@ -51,15 +90,12 @@ compose_project_name="$(
     docker inspect "$(docker-compose ps --quiet db)" \
         --format '{{ index .Config.Labels "com.docker.compose.project"}}'
 )"
-web_image_hash="$(docker-compose images --quiet web | cut -c1-7)"
-dump_filename="psql_dump-$(date --iso-8601=seconds)-${web_image_hash}-${compose_project_name}"
+dump_filename="psql_dump-$(date --iso-8601=minutes)-${compose_project_name}"
 
-echo "Backing up current database..."
-docker-compose exec --user postgres db bash -c '\
-    pg_dump \
-        --dbname $POSTGRES_DB \
-        --no-acl \
-        --no-owner \
-        --encoding utf8 '\
-> "${BACKUPS_DIR}/${dump_filename}.sql"
-echo "Backup written to ${BACKUPS_DIR}/${dump_filename}.sql"
+for service_name in $COMPOSE_PROJECT_SERVICES; do
+    if [ $service_name = fhir ]; then
+        # HAPI db name does not match service name (fhir)
+        service_name=hapifhir
+    fi
+    dump_db $service_name $dump_filename
+done
