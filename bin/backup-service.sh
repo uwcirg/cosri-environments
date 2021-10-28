00bf14d935b33bdd6873de59b526533dde709271
Ivan Cvitkovic <ivanc@uw.edu>
2021-10-28 16:12:03 -0700

    Add comment

--- bin/backup-service.sh
+++ bin/backup-service.sh
@@ -75,6 +75,7 @@ dump_db() {
 
     local POSTGRES_USER="$(docker-compose exec -T $DATABASE_SERVICE_NAME printenv POSTGRES_USER)"
     echo Backing up $db_name...
+    # ACLs are needed by postgrest; do not include `--no-acl`
     docker-compose exec -T --user postgres $DATABASE_SERVICE_NAME bash -c "\
         pg_dump \
             --username $POSTGRES_USER \
